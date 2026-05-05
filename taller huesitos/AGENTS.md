6e85ca74b31f1b26f04e3074f32ffa6845122198
donroberdev <61977947+donroberdev@users.noreply.github.com>
2026-05-05 11:56:28 -0500

    SpriteCharacterController

--- taller huesitos/AGENTS.md
+++ taller huesitos/AGENTS.md
@@ -1,8 +1,14 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: taller huesitos
 - Unity version: Unity 2023.1.0f1
+- Active scene:
+  - Name: NovelaGrafica
+  - Tags:
+    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Ground, Trampas
+  - Layers:
+    - Default, TransparentFX, Ignore Raycast, Water, UI
 - Active game object:
-  - Name: Button (2)
+  - Name: Character
   - Tag: Untagged
-  - Layer: UI
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ taller huesitos/Assets/_Main/Scripts/NovelaGafica/SpriteCharacterController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SpriteCharacterController : MonoBehaviour
+{
+    [Header("Sprite Renderer")]
+    [SerializeField]
+    private SpriteRenderer _mainRender; 
+    [SerializeField]
+    private SpriteRenderer _hairRender;
+    [SerializeField]
+    private SpriteRenderer _tearsRender;
+    [SerializeField]
+    private SpriteRenderer _mouthRender;
+    [SerializeField]
+    private SpriteRenderer _graphic_symbolsRender;
+    [SerializeField]
+    private SpriteRenderer _eyelibsRender;
+    [SerializeField]
+    private SpriteRenderer _pupilRender;
+    [SerializeField]
+    private SpriteRenderer _whiteRender;
+    [SerializeField]
+    private SpriteRenderer _eyebrowsRender;
+    [SerializeField]
+    private SpriteRenderer _cheeksRender;
+    [Header("Sprite")]
+    [SerializeField]
+    private Sprite _mainSprite;
+}
