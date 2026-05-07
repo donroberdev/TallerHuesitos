a6b6ec35259deae15fca14e5a963d6a4ea85d97b
donroberdev <61977947+donroberdev@users.noreply.github.com>
2026-05-07 12:00:49 -0500

    arrays in character component

--- taller huesitos/Assets/_Main/Scripts/NovelaGafica/SpriteCharacterController.cs
+++ taller huesitos/Assets/_Main/Scripts/NovelaGafica/SpriteCharacterController.cs
@@ -2,28 +2,42 @@ using UnityEngine;
 
 public class SpriteCharacterController : MonoBehaviour
 {
-    [Header("Sprite Renderer")]
+    [Header("Array SpriteRender")]
     [SerializeField]
-    private SpriteRenderer _mainRender; 
-    [SerializeField]
-    private SpriteRenderer _hairRender;
-    [SerializeField]
-    private SpriteRenderer _tearsRender;
-    [SerializeField]
-    private SpriteRenderer _mouthRender;
-    [SerializeField]
-    private SpriteRenderer _graphic_symbolsRender;
-    [SerializeField]
-    private SpriteRenderer _eyelibsRender;
-    [SerializeField]
-    private SpriteRenderer _pupilRender;
-    [SerializeField]
-    private SpriteRenderer _whiteRender;
-    [SerializeField]
-    private SpriteRenderer _eyebrowsRender;
-    [SerializeField]
-    private SpriteRenderer _cheeksRender;
+    private SpriteRenderer[] _spriteRenderers;
+
     [Header("Sprite")]
     [SerializeField]
     private Sprite _mainSprite;
+    [SerializeField]
+    private Sprite _hairSprite;
+    [SerializeField]
+    private Sprite _whiteSprites;
+
+    [Header("Array Sprite")]
+    [SerializeField]
+    private Sprite[] _tearSprites;
+    [SerializeField]
+    private Sprite[] _mouthSprites;
+    [SerializeField]
+    private Sprite[] _graphicSprites;
+    [SerializeField]
+    private Sprite[] _eyelibsSprites;
+    [SerializeField]
+    private Sprite[] _pupilSprites;
+    [SerializeField]
+    private Sprite[] _eyebrowsSprites;
+    [SerializeField]
+    private Sprite[] _cheekSprites;
+
+
+
+
+    private void Start()
+    {
+
+    
+    }
+
+
 }
